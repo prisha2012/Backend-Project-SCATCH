2159fb0fc83551cc5f2a5300d34808f613f9ce2d
Prisha <153307711+prisha2012@users.noreply.github.com>
2026-05-20 21:06:19 +0530

    new

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,24 @@
 const express=require('express');
 const router=express.Router();
+const ownerModel=require("../models/owners-model");
 
 router.get("/",(req,res)=>{
     res.send("hey");
 })
+
+if(process.env.NODE_ENV==="development"){
+
+    router.post("/create",async(req,res)=>{
+      let owners= await ownerModel.find();
+      if(owners.length>0)return res.status(500).send("Caanot create");
+      let{fullname,email,password}=req,body;
+     let createdowner= await ownerModel.create({
+    fullname, 
+    email,
+    password
+      })
+        res.status(200).send(createdowner);
+    })
+}
 
 module.exports=router
